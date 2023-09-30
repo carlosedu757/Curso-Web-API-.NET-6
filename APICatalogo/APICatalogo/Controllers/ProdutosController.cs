b7cb181553d91117e87d1c4b13931714166a0933
Carlos Eduardo <cerpcaca2013@gmail.com>
2023-09-30 17:48:20 -0300

    #230930 - Adição do Post, Put e Delete na ProdutosController.

--- APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -27,7 +27,7 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("id:int")]
+        [HttpGet("id:int", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
@@ -38,5 +38,42 @@ namespace APICatalogo.Controllers
             return produto;
         }
 
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest();
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("Obter Produto", new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if(id != produto.ProdutoId)
+                return BadRequest();
+
+            _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+                return NotFound("Produto não encontrado...");
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
     }
 }
