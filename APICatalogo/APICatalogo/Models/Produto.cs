a816595ca8f98e515125222c2964e550725838f4
Carlos Eduardo <cerpcaca2013@gmail.com>
2023-10-08 11:11:03 -0300

    #231008 - Validações e seus tipos.

--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,19 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 
-public class Produto
+public class Produto : IValidatableObject
 {
     public int ProdutoId { get; set; }
 
-    [Required]
-    [StringLength(80)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(80, ErrorMessage = "O nome deve ter entre 5 e 80 caracteres", MinimumLength = 5)]
+    [FirstLetterBig]
     public string? Nome { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(300, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
     public string? Descricao { get; set; }
 
     [Required]
@@ -32,4 +34,27 @@ public class Produto
 
     [JsonIgnore]
     public Categoria Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if(primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new ValidationResult(
+                    "A primeira letra do nome do produto deve ser maiúscula",
+                    new[] { nameof(this.Nome) }
+                    );
+            }
+        }
+
+        if (this.Estoque <= 0)
+        {
+            yield return new ValidationResult(
+                    "O estoque deve ser maior que zero",
+                    new[] { nameof(this.Estoque) }
+                    );
+        }
+    }
 }
